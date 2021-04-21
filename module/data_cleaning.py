58f4c8d8724a7b98e724cf03471769e4c88eaca8
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-04-21 21:45:28 +0900

    중복 조건 추가 및 이상점 코드 주석화
    1. 데이터 클리닝중 중복조건으로 제목 칼럼이 중복 되던 기존의 방식에서 제목과 작성자가 중복된 경우에 데이터가 삭제 되도록 바꿈
    2. 이상점 삭제시 기존의 int,str 오류가 다시 발생하여 주석처리 했음

--- module/data_cleaning.py
+++ module/data_cleaning.py
@@ -1,16 +1,17 @@
 
 def cleaning(dataframe):
-    dataframe.drop_duplicates(['제목'], inplace=True)
+    # 중복되는 게시글 삭제
+    dataframe.drop_duplicates(['제목', '판매자'], inplace=True)
 
     # 제목에 '삽니다' 라는 말이 있으면 삭제
     condition_buy = dataframe['제목'].str.contains('삽니다')
     dataframe.drop(dataframe[condition_buy].index, inplace=True)
 
-    #price이 비이상적인 데이터 삭제하기
-    q1 = dataframe['price'].quantile(0.25)
-    q3 = dataframe['price'].quantile(0.75)
-    iqr = q3 - q1
-    condition = (dataframe['price'] > q3 + 1.5 * iqr) | (dataframe['price'] < q1 - 1.5 * iqr)
-    dataframe.drop(dataframe[condition].index, inplace=True)
+    # #price이 비이상적인 데이터 삭제하기
+    # q1 = dataframe['price'].quantile(0.25)
+    # q3 = dataframe['price'].quantile(0.75)
+    # iqr = q3 - q1
+    # condition = (dataframe['price'] > q3 + 1.5 * iqr) | (dataframe['price'] < q1 - 1.5 * iqr)
+    # dataframe.drop(dataframe[condition].index, inplace=True)
 
     return dataframe
