c4057086e5efd9d5695d8bf41f9ccc2cc9d0d31b
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-04-22 11:49:12 +0900

    'price' 칼럼 문자열 인식 해결
    1. 기존에 csv 파일로 저장하기 전에 dataframe의 가격 칼럼에서 이상점을 찾아 클리닝 하는 과정에서 가격 칼럼을 object로 인식하여 부등호 사용시 오류가 발생했었던 문제를 해결했다.

--- module/main.py
+++ module/main.py
@@ -1,18 +1,25 @@
-
+import pandas as pd
 from selenium_crolling import start
-from data_cleaning import cleaning
+from data_cleaning import cleaning_duplicates, cleaning_Outlier
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # 크롤링 모듈에서 start 함수의 리턴으로 데이터프레임을 가져옴
 df = start()
 
-# cleaning 함수 실행
-df_cleaned = cleaning(df)
+# price 칼럼을 object에서 int로 바꾸기
+df['price'] = df['price'].apply(pd.to_numeric)
+
+# cleaning_duplicates 함수 실행
+df_no_duplicate = cleaning_duplicates(df)
+
+# cleaning_Outlier 함수 실행
+df_no_outer = cleaning_Outlier(df)
 
 # 데이터프레임 저장하기
-df_cleaned.to_csv('/Users/choewonjun/PycharmProjects/study/secondhand/crolling_result.csv', index=False, encoding='utf-8-sig')
+df_no_duplicate.to_csv('/Users/choewonjun/PycharmProjects/study/secondhand/df_result.csv', index=False, encoding='utf-8-sig')
+
 
 # boxplot 그래프 출력하기
-sns.boxplot(y='price', data=df)
+sns.boxplot(y='price', data=df_no_outer)
 plt.show()
